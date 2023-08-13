f71666186d1712a2358695d3ba88d2c4a1d13b54
Nicolas <n.germain.work@gmail.com>
2023-08-13 20:45:41 +0200

    visual reports with Mochawesome

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: 'https://www.homagames.com/',
     viewportWidth: 1920,
     viewportHeight: 1080,
-    supportFile: false // not necessary for this case study
+    supportFile: false, // not necessary for this case study
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
   },
 });
